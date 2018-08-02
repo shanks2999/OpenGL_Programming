9f1299571df75cc006de99e2be7316e715aad0c1
Shanks <shanks2999@outlook.com>
2018-08-02 18:46:54 -0500

    Started basic rendering

---
+++ cmake-build-debug/CMakeFiles/OpenGL_Programming.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_Programming.dir/src/Application.cpp.obj"
+  "OpenGL_Programming.pdb"
+  "OpenGL_Programming.exe"
+  "OpenGL_Programming.exe.manifest"
+  "libOpenGL_Programming.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL_Programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
